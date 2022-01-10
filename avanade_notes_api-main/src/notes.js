f0d849555dbac9685267388d5489100f3bfad557
Juliana Salazar <limajuliana115@yahoo.com.br>
2022-01-10 16:01:06 -0300

    Projeto Notas API

---
+++ avanade_notes_api-main/src/notes.js
@@ -0,0 +1,25 @@
+
+function* generateId() {
+  let id = 1;
+
+  while (true) {
+    yield id++;
+  }
+}
+
+let idGenerator = generateId();
+
+const notes = [
+  {
+    id: idGenerator.next().value,
+    date: new Date(),
+    text: "Minha primeira nota na API"
+  },
+  {
+    id: idGenerator.next().value,
+    date: new Date(),
+    text: "Minha segunda nota na API"
+  },
+];
+
+module.exports = { notes, idGenerator }
